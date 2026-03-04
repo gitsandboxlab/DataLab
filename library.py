af717896ab933c57924cfc035368830c7e5e79c1
gitsandboxlab <alvinks@gmail.com>
2026-03-04 09:11:52 -0800

    Update library.py

--- library.py
+++ library.py
@@ -1,1 +1,9 @@
 # Sample Code
+
+Active Membership
+`GET https://graph.microsoft.com/v1.0/users/{user-id}/transitiveMemberOf/microsoft.graph.group?$select=id,displayName`
+
+
+Get Eligible Memberships
+
+GET https://graph.microsoft.com/v1.0/identityGovernance/privilegedAccess/group/eligibilitySchedules?$filter=principalId eq '{user-id}'
