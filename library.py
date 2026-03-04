8570b263b6e9d43bd5855e24dec8fcf0efdad1f0
gitsandboxlab <alvinks@gmail.com>
2026-03-04 14:27:28 -0800

    Add SQL query for JSON data extraction
    Added SQL query to extract JSON data from SourceTable.

--- library.py
+++ library.py
@@ -1,5 +1,18 @@
 # Sample Code
 
+-- Assuming your table is called 'SourceTable' and column is 'JsonData'
+SELECT 
+    JSON_VALUE(item.value, '$.id') AS PrincipalId, -- If pulling from root
+    App.*
+FROM SourceTable
+CROSS APPLY OPENJSON(JsonData) 
+WITH (
+    id UNIQUEIDENTIFIER '$.id',
+    displayName NVARCHAR(255) '$.displayName',
+    endDateTime DATETIME2 '$.endDateTime',
+    usage NVARCHAR(50) '$.usage'
+) AS App;
+
 Active Membership
 `GET https://graph.microsoft.com/v1.0/users/{user-id}/transitiveMemberOf/microsoft.graph.group?$select=id,displayName`
 
