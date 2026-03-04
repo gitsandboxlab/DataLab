9aee07ddc08d4c6310631d0c148bb12901715677
gitsandboxlab <alvinks@gmail.com>
2026-03-04 09:12:15 -0800

    Remove redundant lines in library.py

--- library.py
+++ library.py
@@ -3,7 +3,5 @@
 Active Membership
 `GET https://graph.microsoft.com/v1.0/users/{user-id}/transitiveMemberOf/microsoft.graph.group?$select=id,displayName`
 
-
 Get Eligible Memberships
-
-GET https://graph.microsoft.com/v1.0/identityGovernance/privilegedAccess/group/eligibilitySchedules?$filter=principalId eq '{user-id}'
+`GET https://graph.microsoft.com/v1.0/identityGovernance/privilegedAccess/group/eligibilitySchedules?$filter=principalId eq '{user-id}'`
