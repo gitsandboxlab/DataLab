ee776a8026099bb3488fc447af1da478d9f0ba74
gitsandboxlab <alvinks@gmail.com>
2026-04-18 11:04:12 -0700

    Replace SigninLogs query with AuditLogs query

--- validation.py
+++ validation.py
@@ -1,8 +1,7 @@
-SigninLogs
-| where UserPrincipalName == "user@yourdomain.com"
-| extend DeviceId = tostring(DeviceDetail.deviceId), 
-         DeviceName = tostring(DeviceDetail.displayName),
-         OS = tostring(DeviceDetail.operatingSystem),
-         Browser = tostring(DeviceDetail.browser)
-| project TimeGenerated, UserPrincipalName, DeviceName, DeviceId, OS, Browser, IPAddress, AppDisplayName
-| order by TimeGenerated desc
+AuditLogs
+| where ActivityDisplayName == "Add user"
+| where TargetResources[0].userPrincipalName == "newuser@yourdomain.com"
+| extend Creator = tostring(InitiatedBy.user.userPrincipalName),
+         CreatorIP = tostring(InitiatedBy.user.ipAddress),
+         AppCreator = tostring(InitiatedBy.app.displayName)
+| project TimeGenerated, OperationName, TargetUser = TargetResources[0].userPrincipalName, Creator, AppCreator, CreatorIP, Result
